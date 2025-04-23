b3f15259978e9934aea99896ee4f8b7aafa1c5b5
xyberii4 <61101529+xyberii4@users.noreply.github.com>
2025-04-23 15:39:31 +0100

    Added method for PlantManage/GetPlantList

--- src/goodwe_client.py
+++ src/goodwe_client.py
@@ -1,9 +1,18 @@
 import requests
 import json
+from typing import Dict, Any, List, Tuple
 
 
 class GoodWeClient:
-    def __init__(self, username, pwd, base_url, default_headers):
+    def __init__(self, username, pwd, base_url, default_headers, log):
+        """
+        Args:
+            username (str): GoodWe account username.
+            pwd (str): GoodWe account password.
+            base_url (str): Base URL of the GoodWe API.
+            default_headers (Dict[str, str]): Default headers.
+            log (Any): Logger instance.
+        """
         self.username = username
         self.pwd = pwd
         self.token = ""
@@ -11,16 +20,31 @@ class GoodWeClient:
 
         self.base_url = base_url
         self.default_headers = default_headers
+        self.log = log
 
-    # Sends request to the GoodWe API
-    def send_request(self, endpoint, headers, data=None, method="POST"):
+        self.log.info("GoodWeClient initialized")
+        self.login()
+
+    def send_request(
+        self, endpoint, headers, version="v3", data=None, method="POST"
+    ) -> Dict[str, Any]:
+        """
+        Sends a request to the GoodWe API.
+        Args:
+            endpoint (str): API endpoint path.
+            headers (Dict[str, str]): Headers for the request.
+            version (str): API version (v2/v3).
+            data (Optional[Dict[str, Any]]): JSON data to send with request.
+            method (str): HTTP method, either "POST" or "GET".
+
+        Returns:
+            JSON response from the API (Dict[str, Any]).
+        """
         try:
-            # Requests aside from CrossLogin require authentication
             if not self.logged_in and endpoint != "/Common/CrossLogin":
                 raise Exception("User not logged in")
 
-            url = f"{self.base_url}{endpoint}"
-            headers = headers | self.default_headers
+            url = f"{self.base_url}{version}{endpoint}"
 
             if method == "POST":
                 response = requests.post(url, headers=headers, json=data)
@@ -29,14 +53,23 @@ class GoodWeClient:
             else:
                 raise ValueError("Unsupported HTTP method")
 
-            response.raise_for_status()  # HTTPError
+            response.raise_for_status()
 
-            return response.json()
+            response = response.json()
+            if response["msg"] in ["success", "Successful"]:
+                return response
+            else:
+                raise Exception(response["msg"])
+
         except Exception as e:
-            raise Exception(f"API error: {e}") from e
+            self.log.exception(f"{version}{endpoint} request failed.", e)
+            raise e
 
-    # Authentication to obtain token
     def login(self):
+        """
+        Retrieves and stores API token.
+        """
+        self.log.info("Logging in to GoodWe API")
         url = "/Common/CrossLogin"
         headers = {
             "token": json.dumps(
@@ -52,11 +85,49 @@ class GoodWeClient:
         }
         data = {"account": self.username, "pwd": self.pwd, "is_local": False}
 
-        response = self.send_request(url, headers, data=data)
-
-        if response["msg"] == "Successful":
-            self.token = response["data"]["token"]
+        try:
+            response = self.send_request(url, headers, data=data)
+            self.token = json.dumps(response["data"])
             self.logged_in = True
-            return
-        else:
-            raise Exception(response["msg"])
+            self.log.info("Login successful")
+        except Exception as e:
+            self.log.exception("Login failed", e)
+            raise e
+
+    def get_plant_list(self) -> Tuple[int, List[Dict[str, Any]]]:
+        """
+        Retreives basic information about all plants. (v2)
+        Returns:
+            Number of plants (int)
+            List of dicts containing plant information (List[Dict[str, Any]])
+                - plantId (str)
+                - plantName (str)
+                - hasInverters (int)
+                - classification (str)
+                - plantTypes : unknown
+                - capacity (str) : kW
+                - batteryCapacity : unknown
+                - createDate (str) : MM/DD/YYYY
+                - latitude (str)
+                - longitude (str)
+
+        """
+        self.log.info("Getting plant list")
+        endpoint = "/PlantManage/GetPlantList"
+        data = {
+            "pager": {
+                "data": {},
+            }
+        }
+
+        headers = self.default_headers.copy()
+        headers["token"] = self.token
+
+        try:
+            response = self.send_request(endpoint, headers, version="v2", data=data)
+            self.log.info("Plant list retrieved successfully")
+            plant_num = response["data"]["page"]["records"]
+            return plant_num, response["data"]["rows"]
+        except Exception as e:
+            self.log.exception("Failed to get plant list", e)
+            raise e

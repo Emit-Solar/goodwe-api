e483c80f776eb76d9a5449b393b57f8d17a348d0
xyberii4 <61101529+xyberii4@users.noreply.github.com>
2025-06-24 17:18:45 +0900

    refactor: centralize client initialization

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
-from goodwe_client import GoodWeClient
-from config import Config
-from logger import Logger
-from database import PostgreSQLClient
-import re
+from .goodwe_client import GoodWeClient
+from .config import Config
+from .logger import Logger
+from .data_processor import GoodweDataProcessor
+from .db_client import SQLClient
 
 
-# Sample usage
-def main():
-    log = Logger("logs/api.log", clear=True)
+def initialization(log_file):
+    log = Logger(f"logs/{log_file}", clear=True)
     api_client = GoodWeClient(
         Config.GOODWE_USERNAME,
         Config.GOODWE_PASSWORD,
@@ -15,36 +14,18 @@ def main():
         Config.DEFAULT_HEADERS,
         log,
     )
-    print(api_client.token)
 
-    db_client = PostgreSQLClient(
-        Config.POSTGRES_IP,
+    sql_client = SQLClient(
+        Config.POSTGRES_DB,
         Config.POSTGRES_USERNAME,
         Config.POSTGRES_PASSWORD,
-        Config.POSTGRES_DB,
+        Config.POSTGRES_IP,
+        Config.POSTGRES_PORT,
         Config.POSTGRES_SCHEMA,
+        Config.POSTGRES_SETUP,
         log,
-        port=Config.POSTGRES_PORT,
     )
 
-    # Init PostGreSQL database with correct columns and tables
-    plant_num, plant_list = api_client.get_plant_list()
-    sample_plant = plant_list[0]
-    sample_monitor_detail = api_client.get_monitor_detail_by_powerstation_id(
-        sample_plant["plantId"]
-    )
-    db_client.create_table("plant_list", sample_plant)
-    db_client.create_table("monitor_detail", sample_monitor_detail)
+    dtp = GoodweDataProcessor(log)
 
-    # Add all plant data to database
-    for i in range(plant_num):
-        # Insert to plant_list table
-        capacity_str = plant_list[i]["capacity"]
-        plant_list[i]["capacity"] = float(
-            "".join(c for c in capacity_str if c.isdigit() or c == ".")
-        )
-        db_client.insert_data("plant_list", plant_list[i])
-
-
-if __name__ == "__main__":
-    main()
+    return log, api_client, sql_client, dtp
